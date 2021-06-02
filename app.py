99595240e6317098f288f8a9efc1b60a4c6e89c3
Meetlowf <tylerm7@vt.edu>
2021-06-02 11:30:25 -0400

    New forms
    New Modals and forms submission

--- app.py
+++ app.py
@@ -7,11 +7,49 @@ from wtforms import StringField
 from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired
 
+import decimal
+import json
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
 app.secret_key = 'bruh'
 db = SQLAlchemy(app)
+
+def convert_sqlobj_json(obj):
+    """Convert the result of the SQLAlchemy query to Json"""
+    recs = []
+    result = [d.__dict__ for d in obj]
+    for r in result:
+        recs.append(convert_one_sqlobj_json(r))
+    return recs
+
+def convert_one_sqlobj_json(r):
+    while "_sa_instance_state" in r:
+        r.pop("_sa_instance_state")
+        for k, v in r.items():
+
+            if isinstance(v, type(datetime.date)):
+                """ Date format """
+                r[k] = v.isoformat()
+
+            elif isinstance(v, str):
+                """ Check for Double quotes, it break the Json format if don't replaced for '\\"' """
+                if '"' in v:
+                    r[k] = v.replace('"', '\\"')
+                elif "\r\n" in v:
+                    r[k] = v.replace("\r\n", "<br>")
+            elif v == None:
+                r[k] = ""
+            elif isinstance(v, decimal.Decimal):
+                r[k] = str(v)
+        return r
+
+# @app.route("<id>")
+# def list_all(id):
+# tasks = Task.query.filter_by(projectid=id).all()
+#     records = convert_sqlobj_json(tasks)
+#     return render_template("tasks.html", records=records)
 
 class ProjectForm(FlaskForm):
     content = StringField('New Project:', validators=[DataRequired()])
@@ -37,6 +75,7 @@ class Tasks(db.Model):
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
     date_due = db.Column(db.String(200), nullable=False)
+    done = db.Column(db.Integer)
 
     def __repr__(self):
         return '<Task %r>' % self.id
@@ -74,8 +113,9 @@ def task(id):
             return 'There was a problem'
     else:
         task_table = Tasks.query.filter_by(project_id=id).all()
+        records = convert_sqlobj_json(task_table)
         projects = Project.query.order_by(Project.date_due).all()
-        return render_template('task.html', tasks=task_table, project=project, projects=projects, form=form)
+        return render_template('task.html', tasks=task_table, project=project, projects=projects, form=form, records=records)
 
 @app.route('/pdelete/<int:id>')
 def pdelete(id):
@@ -97,18 +137,34 @@ def delete(id, pid):
     except: 
         return 'There is a problem'
 
-@app.route('/update/<int:id>/<int:pid>', methods=['GET', 'POST'])
+@app.route('/update/<int:id>/<int:pid>', methods=['POST'])
 def update(id, pid):
     task = Tasks.query.get_or_404(id)
-    if request.method == 'POST':
-        task.content = request.form['content']
+    task.content = request.form['content']
+    try:
+        db.session.commit()
+        return redirect('/task/'+str(pid))
+    except:
+        return 'There is a problem'
+
+@app.route('/done/<int:id>/<int:pid>')
+def done(id, pid):
+    task = Tasks.query.get_or_404(id)
+    if task.done == 1:
+        task.done = 0
         try:
             db.session.commit()
             return redirect('/task/'+str(pid))
         except:
-            return 'problem'
+            return 'There is a problem'
     else:
-        return render_template('update.html', task=task, pid=pid)
+        task.done = 1
+        try:
+            db.session.commit()
+            return redirect('/task/'+str(pid))
+        except:
+            return 'There is a problem'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
