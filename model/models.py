4478b1616dde33ea83ff25ae68e842ad6553217b
Meetlowf <tylerm7@vt.edu>
2021-06-09 15:48:26 -0400

    Fixed glitch
    Fixed a problem with class member not initializing to 0

--- model/models.py
+++ model/models.py
@@ -17,7 +17,7 @@ class Tasks(db.Model):
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
     date_due = db.Column(db.String(200), nullable=False)
-    done = db.Column(db.Integer)
+    done = db.Column(db.Integer, default=0)
 
     def __repr__(self):
         return '<Task %r>' % self.id
